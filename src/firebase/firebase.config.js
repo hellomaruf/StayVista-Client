3f9fbb718eec9c1310e33e2aa4925ec8d6e97eec
maruf-ahmed <marufahamed1110@gmail.com>
2024-05-18 17:25:07 +0600

    First commit with basic setup

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCbc6ebNURWYn4OEbo5Xt2-pSQBphIgLAY",
+  authDomain: "stayvista-9d567.firebaseapp.com",
+  projectId: "stayvista-9d567",
+  storageBucket: "stayvista-9d567.appspot.com",
+  messagingSenderId: "356727225332",
+  appId: "1:356727225332:web:5772509b4ea2b076df97a1",
+  measurementId: "G-HBTBEQS67J"
+};
+export const app = initializeApp(firebaseConfig)
