2ce3f99218ed66d7d972131c0df1a499bf5f1e17
maruf-ahmed <marufahamed1110@gmail.com>
2024-05-22 14:44:42 +0600

    Added some problem solving

--- src/pages/Dashboard/AddRoom.jsx
+++ src/pages/Dashboard/AddRoom.jsx
@@ -1,10 +1,20 @@
+import { useState } from "react";
 import AddRoomForm from "../../components/Dashboard/AddRoomForm";
 
 function AddRoom() {
+  const [dates, setDates] = useState({
+    startDate: new Date(),
+    endDate: null,
+    key: "selection",
+  });
+  const handleDates = (item) => {
+    console.log(item);
+    setDates(item.selection);
+  };
+  console.log(dates);
   return (
     <div>
-     
-      <AddRoomForm />
+      <AddRoomForm dates={dates} handleDates={handleDates} />
     </div>
   );
 }
